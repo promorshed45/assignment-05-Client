8bf4e3b67e58bc513df576ed2de40ae2cb735529
Morshed Alam <ctgmorshed45@gmail.com>
2024-09-05 19:14:57 +0600

    payment system successfully done

--- src/components/Dashboard/AdminDashboard/AdminMenu.tsx
+++ src/components/Dashboard/AdminDashboard/AdminMenu.tsx
@@ -10,35 +10,35 @@ const AdminMenu = () => {
                         <label className="px-3 text-xs font-semibold uppercase text-gray-500">Admin Dashboard</label>
                         <Link
                             to='/dashboard'
-                            className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                            className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
                         >
                             <LayoutDashboardIcon className="h-5 w-5 text-gradient-to-r from-rose-400 to-red-500" aria-hidden="true" />
                             <span className="mx-2 pl-3 text-sm font-medium">Dashboard</span>
                         </Link>
                         <Link
                             to='/dashboard/service-management'
-                            className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                            className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
                         >
                             <CarTaxiFrontIcon className="h-5 w-5 text-gradient-to-r from-indigo-500 to-purple-500" aria-hidden="true" />
                             <span className="mx-2 pl-3 text-sm font-medium">Service </span>
                         </Link>
                         <Link
                             to='/dashboard/slots-management'
-                            className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                            className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
                         >
                             <Calendar className="h-5 w-5 text-gradient-to-r from-indigo-500 to-purple-500" aria-hidden="true" />
                             <span className="mx-2 pl-3 text-sm font-medium">Slot </span>
                         </Link>
                         <Link
                             to='/dashboard/users-management'
-                            className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                            className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
                         >
                             <User2Icon className="h-5 w-5 text-gradient-to-r from-indigo-500 to-purple-500" aria-hidden="true" />
                             <span className="mx-2 pl-3 text-sm font-medium">User </span>
                         </Link>
                         <Link
                             to='/dashboard/booking-management'
-                            className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                            className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
                         >
                             <Wallet className="h-5 w-5 text-gradient-to-r from-indigo-500 to-purple-500" aria-hidden="true" />
                             <span className="mx-2 pl-3 text-sm font-medium">Booking </span>
--- src/components/Dashboard/UserDashboard/UserMenu.tsx
+++ src/components/Dashboard/UserDashboard/UserMenu.tsx
@@ -10,7 +10,7 @@ const UserMenu = () => {
                             <label className="px-3 text-xs font-semibold uppercase text-gray-500">User Dashboard</label>
                             <Link
                                 to='/dashboard'
-                                className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                                className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 hover:text-gray-900"
                             >
                                 <LayoutDashboardIcon className="h-5 w-5 text-gradient-to-r from-rose-400 to-red-500" aria-hidden="true" />
                                 <span className="mx-2 text-sm font-medium">Dashboard</span>
@@ -20,21 +20,21 @@ const UserMenu = () => {
                         <div className="space-y-3">
                             <label className="px-3 text-xs font-semibold uppercase text-gray-500">Content</label>
                             <a
-                                className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                                className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 hover:text-gray-900"
                                 href="#"
                             >
                                 <Newspaper className="h-5 w-5 text-gradient-to-r from-green-400 to-teal-500" aria-hidden="true" />
                                 <span className="mx-2 text-sm font-medium">Blogs</span>
                             </a>
                             <a
-                                className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                                className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 hover:text-gray-900"
                                 href="#"
                             >
                                 <BellRing className="h-5 w-5 text-gradient-to-r from-yellow-400 to-orange-500" aria-hidden="true" />
                                 <span className="mx-2 text-sm font-medium">Notifications</span>
                             </a>
                             <a
-                                className="flex items-center rounded-lg px-3 py-2 dark:text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 dark:hover:text-gray-900"
+                                className="flex items-center rounded-lg px-3 py-2 text-gray-300 transition transform duration-300 hover:bg-gray-200 hover:scale-105 hover:text-gray-900"
                                 href="#"
                             >
                                 <Settings className="h-5 w-5 text-gradient-to-r from-blue-400 to-cyan-500" aria-hidden="true" />
